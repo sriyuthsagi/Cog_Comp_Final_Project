e1e4334b21987db9bb7773e1669d6a11c81b8b50
sriyuthsagi <42911878+sriyuthsagi@users.noreply.github.com>
2019-12-04 13:42:20 -0500

    Create connect_rabbitmq.py

---
+++ connect_rabbitmq.py
@@ -0,0 +1,48 @@
+import pika
+import json
+
+from agent import Agent
+
+# REMEMBER TO ASK::: msg.sender? or other?
+
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters(host='128.113.21.86',
+                                       virtual_host='/',
+                                       credentials=credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+agent = Agent("Watson", 1001)
+
+def connect_to_server():
+   
+    channel.queue_declare(queue='to-agents')
+    channel.queue_declare(queue='output-gate')
+   
+    #channel.exchange_declare(exchange='amq.topic',
+                             #type='direct', durable=True)
+   
+    channel.queue_bind(exchange='amq.topic',
+                       queue='to-agents')
+    channel.queue_bind(exchange='amq.topic',
+                       queue='output-gate')
+    return channel
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+      
+    msg = json.loads(body);  
+    if msg["msgType"] == "setAgentUtility":
+      #send to the class to create the initial values for the products
+    else:
+      #send to the rest of the program to get reply
+      
+    ch.basic_publish(exchange='amq.topic', routing_key='output-gate', body=json.dumps(reply));
+connect_to_server();
+
+    #channel.queue_declare(queue='amq.topic')
+channel.basic_consume(
+    queue='to-agents', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
